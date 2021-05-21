8cd3e4ed47bd2dd78869683571fd3b9420c96c58
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2021-05-21 14:58:42 -0700

    Partitions 4 correctly now.

--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,5 @@
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class Solution
 {
@@ -61,37 +58,47 @@ public class Solution
         partitions.add(currPartition);
         while(valueAtIndex != 1)
         {
-            indexPicked = FindLargestNonOneIndex(currPartition);
-            valueAtIndex = currPartition[indexPicked];
             currPartition = FindPartitionSuccessor(currPartition, indexPicked);
             partitions.add(currPartition);
+            indexPicked = FindLargestNonOneIndex(currPartition);
+            valueAtIndex = currPartition[indexPicked];
         }
-        return (int[][]) partitions.toArray();
+        return convertIntegers(partitions);
     }
-
+    public static int[][] convertIntegers(ArrayList<int[]> integers)
+    {
+        int[][] result = new int[integers.size()][];
+        for (int i=0; i < result.length; i++)
+        {
+            int[] ret = integers.get(i);
+            result[i] = ret;
+        }
+        return result;
+    }
     private static int[] FindPartitionSuccessor(int[] currPartition, int indexPicked)
     {
         int[] newPartition;
-        if(indexPicked == currPartition.length-1 || indexPicked != currPartition.length-2
-                || (indexPicked == currPartition.length-2 && currPartition[indexPicked] - 1 <= currPartition[indexPicked+1]+1))
+        if(indexPicked == currPartition.length-2 && currPartition[indexPicked]-1 >= currPartition[indexPicked+1]+1)
+        {
+            newPartition = currPartition.clone();
+            newPartition[indexPicked] = currPartition[indexPicked]-1;
+            newPartition[indexPicked+1] += 1;
+        }
+        else
         {
             newPartition = new int[currPartition.length + 1];
             newPartition[indexPicked] = currPartition[indexPicked]-1;
             newPartition[indexPicked+1] = 1;
-            for(int i = indexPicked+1; i < currPartition.length; i++)
+            if(indexPicked < currPartition.length-2)
             {
-                newPartition[i+2] = currPartition[i];
+                for (int i = indexPicked + 1; i < currPartition.length; i++) {
+                    newPartition[i + 1] = currPartition[i];
+                }
             }
             for(int i = 0; i < indexPicked;i++)
             {
                 newPartition[i] = currPartition[i];
             }
-        }
-        else
-        {
-            newPartition = currPartition.clone();
-            newPartition[indexPicked] = currPartition[indexPicked]-1;
-            newPartition[indexPicked+1] += 1;
         }
         return newPartition;
     }
@@ -100,7 +107,7 @@ public class Solution
     {
         int currentVal = 1;
         int indexChosen = 0;
-        for(int i = currPartition.length-1; i > 0 && indexChosen == 0; i++)
+        for(int i = currPartition.length-1; i >= 0 && indexChosen == 0; i--)
         {
             currentVal = currPartition[i];
             if(currentVal > 1)
